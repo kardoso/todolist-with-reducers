a5b41b4eff2d7b0612d249b36c01911106fb41b5
Samuel Cardoso <scksamuel.sc@gmail.com>
2020-04-07 10:23:55 -0300

    Add react for showing the UI

---
+++ react-ui.js
@@ -0,0 +1,42 @@
+function List(props) {
+  return (
+    <ul>
+      <li>List</li>
+    </ul>
+  )
+}
+
+class Todos extends React.Component {
+  render() {
+    return (
+      <div>
+        TODOS
+        <List />
+      </div>
+    )
+  }
+}
+
+class Goals extends React.Component {
+  render() {
+    return (
+      <div>
+        GOALS
+        <List />
+      </div>
+    )
+  }
+}
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Todos />
+        <Goals />
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'))
